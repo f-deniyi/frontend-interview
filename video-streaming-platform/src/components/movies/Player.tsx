12a697728d6e8c051c17fce6eb8d42201e0be4e5
f-deniyi <holarfemilekan049@gmail.com>
2024-11-27 01:55:38 +0000

    Added video player

---
+++ video-streaming-platform/src/components/movies/Player.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import useFetchTrailer from '../../hooks/useFetchTrailer';
+import { Modal, CircularProgress } from '@mui/material';
+
+interface PlayerProps {
+    movieId: number;
+    isOpen: boolean;
+    onClose: () => void;
+}
+
+const Player: React.FC<PlayerProps> = ({ movieId, isOpen, onClose }) => {
+    const { trailerUrl, thumbnailUrl, isLoading, error } = useFetchTrailer(movieId);
+
+    return (
+        <Modal open={isOpen} onClose={onClose} className='bg-black bg-opacity-70 backdrop-blur-lg'>
+            <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black rounded-md overflow-hidden shadow-lg w-[90%] h-[80%]">
+                {isLoading ? (
+                    <div className="flex items-center justify-center h-full">
+                        <CircularProgress />
+                    </div>
+                ) : trailerUrl ? (
+                    <>
+                        <div className="relative w-full h-full">
+                            <iframe
+                                width="100%"
+                                height="100%"
+                                src={trailerUrl}
+                                title="Movie Trailer"
+                                frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen
+                            ></iframe>
+                            <img
+                                src={thumbnailUrl || 'https://via.placeholder.com/300'}
+                                alt="Trailer Thumbnail"
+                                className="absolute top-0 left-0 w-full h-full object-cover opacity-50"
+                            />
+                        </div>
+                    </>
+                ) : (
+                    <p className="text-white text-center mt-4">{error || 'Trailer not available'}</p>
+                )}
+            </div>
+        </Modal>
+    );
+};
+
+export default Player;
