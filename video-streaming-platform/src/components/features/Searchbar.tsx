68ad2a9bccff70f85a2a9b71859977e878aba43c
f-deniyi <holarfemilekan049@gmail.com>
2024-11-27 01:57:48 +0000

    search video functionality

--- video-streaming-platform/src/components/features/Searchbar.tsx
+++ video-streaming-platform/src/components/features/Searchbar.tsx
@@ -1,30 +1,36 @@
 import React, { useState, useEffect } from 'react';
+import SearchIcon from '@mui/icons-material/Search';
 
 const SearchBar = ({ onSearch }: { onSearch: (query: string) => void }) => {
     const [searchTerm, setSearchTerm] = useState('');
     const [debouncedTerm, setDebouncedTerm] = useState('');
 
+    
     useEffect(() => {
         const timer = setTimeout(() => {
             setDebouncedTerm(searchTerm);
-        }, 500);
+        }, 500); 
 
         return () => clearTimeout(timer);
-    }, [searchTerm]);
+    }, [searchTerm]); 
 
-  
     useEffect(() => {
-        onSearch(debouncedTerm);
-    }, [debouncedTerm, onSearch]);
+        if (debouncedTerm) {
+            onSearch(debouncedTerm); 
+        }
+    }, [debouncedTerm, onSearch]); 
 
     return (
-        <input
-            type="text"
-            placeholder="Search..."
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            className="p-2 border rounded w-full"
-        />
+        <div className="relative w-[500px] mx-auto bg-white dark:bg-[#181818ec] rounded-full border border-gray-300 dark:border-gray-600 outline-none dark:focus-within:border-gray-300 focus-within:border-gray-600 transition-all ease-in-out">
+            <SearchIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 dark:text-gray-400" />
+            <input
+                type="text"
+                placeholder="What do you want to watch?"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                className="w-[500px] py-2 pl-10 pr-4 bg-transparent border-none text-gray-900 dark:text-gray-100 rounded-full outline-none focus"
+            />
+        </div>
     );
 };
 
