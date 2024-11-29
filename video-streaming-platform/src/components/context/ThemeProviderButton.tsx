110bbc252bbbe6aa69edf631fe2c83932f1bd7b4
f-deniyi <holarfemilekan049@gmail.com>
2024-11-29 20:23:15 +0000

    changed theme icons

--- video-streaming-platform/src/components/context/ThemeProviderButton.tsx
+++ video-streaming-platform/src/components/context/ThemeProviderButton.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useThemeContext } from '../../context/ThemeProvider';
 import { FaMoon, FaSun } from 'react-icons/fa';
+import { TbSunFilled } from "react-icons/tb";
+import { BsFillMoonStarsFill } from "react-icons/bs"
 
 const ThemeToggleButton = () => {
     const { theme, toggleTheme } = useThemeContext();
@@ -12,9 +14,20 @@ const ThemeToggleButton = () => {
             className="p-2 rounded-full transition-transform duration-500 ease-in-out"
         >
             {theme === 'light' ? (
-                <FaMoon className="text-yellow-500 text-2xl transform scale-125" />
+                <span
+                    className={` transition-opacity duration-500 ease-in-out ${theme === 'light' ? 'opacity-100' : 'opacity-0'
+                        }`}
+                >
+                    <TbSunFilled className="text-yellow-500 text-2xl transform scale-125" />
+                </span>
             ) : (
-                <FaSun className="text-yellow-300 text-2xl transform scale-125" />
+                <span
+                className={` transition-opacity duration-500 ease-in-out ${
+                    theme === 'dark' ? 'opacity-100' : 'opacity-0'
+                }`}
+            >
+                <BsFillMoonStarsFill className="text-yellow-300 text-2xl transform scale-125" />
+                </span>
             )}
         </button>
     );
